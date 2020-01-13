c5b01b111b548c0f58484564e5bbdd8e31f12af8
Ali <ali1195shah@gmail.com>
2020-01-13 01:28:17 -0500

    (CR)U(D)

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -10,8 +10,12 @@ class SuperherosController < ApplicationController
     end
 
     def create
-        @super = Superhero.create(super_params)
-        render json: @super
+        @Super = Superhero.create(super_params)
+        if @Super.valid?
+            render json: @Super, status: :created
+        else
+            render json: {errors: @Super.errors.full_messages}, status: 400
+        end
     end
 
     def update
@@ -27,7 +31,34 @@ class SuperherosController < ApplicationController
     private
 
     def super_params
-        params.permit(:name)
+            params.permit(:name)
+                # :intelligence,
+                # :strength, 
+                # :speed, 
+                # :durability, 
+                # :power, 
+                # :combat, 
+                # :full_name,
+                # # :alter_ego,
+                # :aliases,
+                # :place_of_birth,
+                # # :first_appearance,
+                # # :publisher,
+                # # :alignment,
+                # # :gender,
+                # :race, 
+                # # :height_feet, 
+                # # :height_meters,
+                # # :weight_lbs,
+                # # :weight_kgs,
+                # :eye_color, 
+                # :hair_color, 
+                # :occupation, 
+                # # :base,
+                # :group_affiliation, 
+                # :relatives,
+                # # :image_url
+        # )
     end
     
 end
