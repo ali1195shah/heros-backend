f642c00822a1965d86c725f6c6f144232c4118b8
Ali <ali1195shah@gmail.com>
2020-02-06 22:15:13 -0500

    ramdom

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
 
         if user && user.authenticate(params[:password])
             
-            render json: { token: token(user.id), user_id: user.id } #orders: user.cart.last.showPurchaseMovies, user_id: user.id, cart_id: user.carts.last.id}
+            render json: { token: token(user.id), user_id: user.id, userName: user.username } #orders: user.cart.last.showPurchaseMovies, user_id: user.id, cart_id: user.carts.last.id}
         else
             
             render json: { errors: ["That didn't match any users WE know about 💁" ]}, status: :unprocessable_entity
